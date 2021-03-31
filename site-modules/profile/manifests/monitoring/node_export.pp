8fe143b8f3e0c6171af37790a6213e4c4e79dca0
Kevin Diaz <kevrocks67@gmail.com>
2021-03-31 01:16:57 -0400

    Add firewall opening for node_exporter

--- site-modules/profile/manifests/monitoring/node_export.pp
+++ site-modules/profile/manifests/monitoring/node_export.pp
@@ -1,3 +1,8 @@
 class profile::monitoring::node_export {
   include prometheus::node_exporter
+  firewalld_port { 'node_exporter':
+    ensure   => present,
+    port     => 9100,
+    protocol => 'tcp',
+  }
 }
