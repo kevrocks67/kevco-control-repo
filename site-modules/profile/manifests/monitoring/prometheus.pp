8fe7ea01b7800a79288edff2302aeccc6e7324e8
Kevin Diaz <kevrocks67@gmail.com>
2021-03-31 00:59:08 -0400

    Create prometheus profile

---
+++ site-modules/profile/manifests/monitoring/prometheus.pp
@@ -0,0 +1,23 @@
+class profile::monitoring::prometheus {
+  version        => lookup('profile::prometheus::version'),
+  scrape_configs => [
+    {
+      'job_name'        => 'prometheus',
+      'scrape_interval' => lookup('profile::prometheus::prom_scrape_int', undef, undef, '5s')
+      'scrape_timeout'  => lookup('profile::prometheus::prom_scrape_to', undef, undef, '5s')
+      'static_configs'  => [
+        { 'targets' => [ "${facts['networking']['interfaces']['eth0']['ip']}:9090" ] },
+      ],
+    },
+    {
+      'job_name'        => 'node_exporter',
+      'scrape_interval' => lookup('profile::prometheus::node_scrape_int', undef, undef, '5s'),
+      'scrape_timeout'  => lookup('profile::prometheus::node_scrape_to', undef, undef, '5s'),
+      'static_configs'  => [
+        {
+          'targets' => lookup('profile::prometheus::node_export_targets')
+        },
+      ],
+    },
+  ],
+}
